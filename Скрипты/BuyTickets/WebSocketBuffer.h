e6906b99bbf65a81e22685f110b9007576ffc6f8
GeT <Artem-klevtsov@mail.ru>
2024-10-01 16:23:15 +0300

    Скрипты

---
+++ Скрипты/BuyTickets/WebSocketBuffer.h
@@ -0,0 +1,10 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"1cc9a7f9309345c6aa7825c1f218fe20\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 113 bytes)
+
+#endif
---
+++ Скрипты/Log_LogOut/WebSocketBuffer.h
@@ -0,0 +1,11 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"fd073b07da7d46b8b865c52500f1b94a\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{\"remote-settings/monitor_changes\""
+                        ":\"\\\"1727734884245\\\"\"}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 166 bytes)
+
+#endif
